8530838206354ca0452fb8ad7b8240b3a944c151
Mousumi Kumari <mousumi.kumari@modmed.com>
2023-03-25 13:25:20 +0530

    first commit

---
+++ DDSAD/DDSAD/Factorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDSAD
+{
+    public class Factorial
+    {
+        public int CalculateFactorial (int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * CalculateFactorial(n - 1);
+        }
+    }
+}
---
+++ DDSAD/DDSAD/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DDSAD
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculate Factorial
+            Console.Write("Enter any number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Factorial f = new Factorial();
+            int ans = f.CalculateFactorial(n);
+            Console.WriteLine("The Factorial is : " + ans);
+            Console.ReadLine();
+        }
+    }
+}
